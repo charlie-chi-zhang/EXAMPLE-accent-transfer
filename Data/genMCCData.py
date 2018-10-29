13dee42815abe11818cb234b3930f24c0f085ef1
Patrick Wang <pwang21@mit.edu>
2018-10-29 14:45:16 -0400

    cleaned mfcc gen, started cnn

--- Data/genMCCData.py
+++ Data/genMCCData.py
@@ -5,35 +5,45 @@ from fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 import matplotlib.pyplot as plt
 
-mccs = []
+def get_aligned_mfccs(file_pair):
+    """
+    Takes a pair of file paths, returns aligned MFCC coefficients for the two
+    audio clips
+    """
+    mfccs = []
+    
+    for file in file_pair:  
+    
+        sound = AudioSegment.from_file(file)   
+        samples = np.array(list(sound.get_array_of_samples()))
+    
+        mfcc_samples = psf.mfcc(samples,
+                    samplerate=16000,
+                    winlen=0.025,
+                    winstep=0.01,
+                    numcep=13,
+                    nfilt=26,
+                    nfft=512,
+                    lowfreq=0,
+                    highfreq=None,
+                    preemph=0.97,
+                    ceplifter=22,
+                    appendEnergy=True)
+    
+        mfccs.append(mfcc_samples)
+    
+    sample = mfccs[0]
+    target = mfccs[1]
+    
+    distance, path = fastdtw(sample, target, dist=euclidean)
+    
+    sample_aligned = [sample[i] for (i, j) in path]
+    target_aligned = [target[j] for (i, j) in path]
+    
+    return (np.array(sample_aligned), np.array(target_aligned))
 
-for file in ["cuts/bosnian3/Wednesday.mp3","cuts/english368/Wednesday.mp3"]:
+def reconstruct_from_mfcc(coeffs):
+    pass
 
-	sound = AudioSegment.from_file(file)
 
-	# this is an array
-	samples = np.array(list(sound.get_array_of_samples()))
-
-	mcc = psf.mfcc(samples,
-					samplerate=16000,
-					winlen=0.025,
-					winstep=0.01,
-					numcep=13,
-	                nfilt=26,
-	                nfft=512,
-	                lowfreq=0,
-	                highfreq=None,
-	                preemph=0.97,
-	     			ceplifter=22,
-	     			appendEnergy=True)
-
-	mccs.append(mcc)
-
-x = mccs[0]
-y = mccs[1]
-
-distance, path = fastdtw(x, y, dist=euclidean)
-
-print(y.shape)
-
-print(path)
+print(get_aligned_mfccs(("cuts/bosnian3/Wednesday.mp3","cuts/english368/Wednesday.mp3")))
---
+++ Data/mfcc_cnn.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 14:27:18 2018
+
+@author: Patrick
+"""
+
+import tensorflow as tf
+
+learning_rate = 0.0001
+epochs = 10
+batch_size = 50
+
+coef_num = 13
+samples = 200
+
+x = tf.placeholder(tf.float32, [None, coef_num, samples])
+y = tf.placeholder(tf.float32, [None, coef_num, samples])
+
+def create_new_conv_layer(input_data, num_input_channels, num_filters, filter_shape, pool_shape, name):
+    # setup the filter input shape for tf.nn.conv_2d
+    conv_filt_shape = [filter_shape, num_input_channels,
+                      num_filters]
+
+    # initialise weights and bias for the filter
+    weights = tf.Variable(tf.truncated_normal(conv_filt_shape, stddev=0.03),
+                                      name=name+'_W')
+    bias = tf.Variable(tf.truncated_normal([num_filters]), name=name+'_b')
+
+    # setup the convolutional layer operation
+    out_layer = tf.nn.conv2d(input_data, weights, [1, 1, 1, 1], padding='SAME')
+
+    # add the bias
+    out_layer += bias
+
+    # apply a ReLU non-linear activation
+    out_layer = tf.nn.relu(out_layer)
+
+    # now perform max pooling
+    ksize = [1, pool_shape[0], pool_shape[1], 1]
+    strides = [1, 2, 2, 1]
+    out_layer = tf.nn.max_pool(out_layer, ksize=ksize, strides=strides, 
+                               padding='SAME')
+
+    return out_layer
